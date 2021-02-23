49f8257929e3c8c7ad82f66259be28b50e61b294
aidelojep <57565189+aidelojep@users.noreply.github.com>
2021-02-23 06:11:52 +0100

    Add files via upload

---
+++ StudentTest.java
@@ -0,0 +1,26 @@
+public class StudentTest {
+ public static void main(String[] args) {
+
+Student myGrade1 = new Student("Peter", 94.70);
+Student myGrade2 = new Student("BlackMan", 71.09);
+  
+ System.out.printf("%s's letter grade is: %s%n", myGrade1.getName(),myGrade1.getLetterGrade());
+ System.out.printf("%s's letter grade is: %s%n", myGrade2.getName(), myGrade2.getLetterGrade()); 
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
