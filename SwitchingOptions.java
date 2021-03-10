6ec4712c0d7fbfc01b59821795b1430c9d26ea6f
WirelessAPI <57565189+aidelojep@users.noreply.github.com>
2021-03-10 11:39:14 +0100

    Add files via upload
    Soooooooooooooooooo, this buzz word SWITCH LOOP is just simply a glorified if statement, i meannnnnnnnnnnnnnnnn!!!  It took me less than 5minsutes to realize this...and now it's my FAV!!

---
+++ SwitchingOptions.java
@@ -0,0 +1,20 @@
+public class SwitchingOptions{
+    public static void main(String[] args) {
+        int i = (0);
+
+        switch(i) {
+            case 0:
+                System.out.println("Print one!");
+             break;
+            case 1:
+                System.out.println("Print two!");
+             break;
+            case 2:
+                System.out.println("Print three");
+                break;
+            default:
+                System.out.println("wtf man??");
+                break;
+        }
+    }
+}
