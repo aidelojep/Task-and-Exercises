72677c5112362de7d9b57e2ec74eb949f83663d4
WirelessAPI <57565189+aidelojep@users.noreply.github.com>
2021-03-14 01:09:38 +0100

    Add files via upload
    I'm just trynna catch fun with ConditionalOperator.

---
+++ ConditionalOperator.java
@@ -0,0 +1,8 @@
+public class ConditionalOperator{
+    public static void main(String[] args) {
+        int grade=50;
+        System.out.println(grade>= 60 ? "Lekwa will soon blow": "Lekwa has made it!!");
+
+
+    }
+}
