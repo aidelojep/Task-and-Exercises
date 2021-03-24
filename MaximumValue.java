31de3017d73368454d75718cf16d12a04b866227
WirelessAPI <57565189+aidelojep@users.noreply.github.com>
2021-03-24 08:48:42 +0100

    Add files via upload
    It's funny how my colleagues made a joke about maximumValue,....it's  becoming more clearer right now...lol

---
+++ MaximumValue.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class MaxiumValue{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter three main values ");
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+
+        double result = maximum(number1, number2, number3);
+        System.out.println("The maximum value is:" + result);
+    }
+        public static double maximum(double x,  double y, double z) {
+            double maximumvalue =x;
+            if (y > maximumvalue)
+                maximumvalue = y;
+            if (z > maximumvalue)
+                maximumvalue = z;
+
+            return maximumvalue;
+        }
+
+
+
+
+    }
